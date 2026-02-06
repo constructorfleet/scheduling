7d3dc7afd6df12a26b1cf121dd3bbca6f125c05c
Teagan Glenn <teagan@extrahop.com>
2026-02-06 16:50:33 -0700

    Update generated code

---
+++ apps/api/src/prisma.d.ts
@@ -0,0 +1,17 @@
+declare module "../generated/prisma-sqlite" {
+  export class PrismaClient {
+    constructor(...args: unknown[]);
+  }
+}
+
+declare module "../generated/prisma-postgres" {
+  export class PrismaClient {
+    constructor(...args: unknown[]);
+  }
+}
+
+declare module "../generated/prisma-mysql" {
+  export class PrismaClient {
+    constructor(...args: unknown[]);
+  }
+}
